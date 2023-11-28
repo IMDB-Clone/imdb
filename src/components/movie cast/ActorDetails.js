e59d7b60ca97b97d484a476c734191e2a302e958
aad4000 <118906210+aad405@users.noreply.github.com>
2023-11-28 12:04:48 +0200

    Refine styling for 'Known For' section and integrate with main branch

--- src/components/movie cast/ActorDetails.js
+++ src/components/movie cast/ActorDetails.js
@@ -27,25 +27,26 @@ const ActorDetails = () => {
     <div className="actor-details-container">
       {/* Actor's header information */}
       {actorDetails && (
-        <div className="actor-header">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${actorDetails.profile_path}`}
-            alt={actorDetails.name}
-            className="actor-photo"
-          />
+    <div className="actor-header">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${actorDetails.profile_path}`}
+        alt={actorDetails.name}
+        className="actor-photo"
+      />
           <div>
-            <h1 className="actor-name">{actorDetails.name}</h1>
-            <p className="actor-bio">{actorDetails.biography}</p>
-            <p className="actor-dob">Born: {actorDetails.birthday}</p>
+          <h1 className="actor-name">{actorDetails.name}</h1>
+        <p className="actor-bio">{actorDetails.biography}</p>
+        <p className="actor-dob">Born: {actorDetails.birthday}</p>
+            {/* Assuming .actor-dob is defined in your CSS */}
           </div>
         </div>
       )}
 
       {/* Known For section */}
-      <div className="known-for-container">
-        <h2 className="known-for-title">Known For</h2>
-        <div className="known-for-grid">
-          {actorMovies.map((movie) => (
+      <div className="known-for-section">
+    <h2 className="known-for-title">Known For</h2>
+    <div className="known-for-grid">
+      {actorMovies.map((movie) => (
             <div key={movie.id} className="movie-item">
               <div className="movie-poster-container">
                 <img
@@ -56,12 +57,13 @@ const ActorDetails = () => {
               </div>
               <div className="movie-details">
                 <p className="movie-title">{movie.title}</p>
-                <p className="movie-character">{movie.character}</p>
-                <p className="movie-year">{new Date(movie.release_date).getFullYear()}</p>
-                <div className="movie-rating">
-                  <span className="rating-icon">★</span>
-                  <span className="rating">{movie.vote_average.toFixed(1)}</span>
-                </div>
+                <p className="movie-rating">
+                  <span className="rating-star">★</span> {/* Assuming you have a star icon */}
+                  {movie.vote_average}
+                </p>
+                <p className="movie-role">{movie.character}</p>
+                <p className="movie-year">{movie.release_date.split('-')[0]}</p>
+                {/* Assuming release_date is in 'YYYY-MM-DD' format */}
               </div>
             </div>
           ))}
