7a958827e9f6ec060e148c8bff54fc4de1fefdc4
JJoellee <joelleelhomsi@gmail.com>
2023-12-03 23:22:34 +0200

    Design fix

--- src/containers/MovieReview/MovieReviewing.js
+++ src/containers/MovieReview/MovieReviewing.js
@@ -1,23 +1,24 @@
-import {React, useContext} from "react";
+import { React, useContext } from "react";
 import MovieYearTitleComponent from "../../components/MovieYearTitle/MovieYearTitleComponent";
 import MovieCounterAndReviews from "../../components/MovieYearTitle/MovieCounterAndReviews";
 import "./MovieReviewing.css"; // Ensure you have this CSS file
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
-import { UserContext } from '../../services/usercontext';
+import { UserContext } from "../../services/usercontext";
 
 const MovieReviewing = () => {
   const { user } = useContext(UserContext);
   const sessionID = user?.tmdbSessionId;
-  
+
   const { movieId, Session_ID } = useParams();
   return (
-    <div className="movie-reviewing">
-      <NavBar />
-      <MovieYearTitleComponent movieId={movieId} Session_ID={sessionID} />
-      <MovieCounterAndReviews movieId={movieId} />
+    <div>
+            <NavBar />
+      <div className="movie-reviewing">
+        <MovieYearTitleComponent movieId={movieId} Session_ID={sessionID} />
+        <MovieCounterAndReviews movieId={movieId} />
+      </div>
     </div>
-    
   );
 };
 
